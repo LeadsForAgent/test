77c13929b47160973662a8533abe643490b27580
LeadsFor Agent <gouthamnair@leadsforagent.com>
2026-02-02 21:32:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {base: '/for-my-valentine/',
+    return {base: './',
+,
 
       server: {
         port: 3000,
